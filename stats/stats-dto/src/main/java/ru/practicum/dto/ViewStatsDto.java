b0d15387508f06a31cca1f480d2f67d8b0e6ee38
Igor0390 <118315204+Igor0390@users.noreply.github.com>
2023-09-14 18:31:11 +0200

    Изменения после ревью

--- stats/stats-dto/src/main/java/ru/practicum/dto/ViewStatsDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/dto/ViewStatsDto.java
@@ -5,14 +5,9 @@ import lombok.*;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ViewStatsDto {
     private String app;
     private String uri;
     private Long hits;
-
-    public ViewStatsDto(String app, String uri, Long hits) {
-        this.app = app;
-        this.uri = uri;
-        this.hits = hits;
-    }
 }
