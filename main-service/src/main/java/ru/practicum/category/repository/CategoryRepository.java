92a711f33396034a960d3fb5d59bdfe8cc3543d3
Igor0390 <118315204+Igor0390@users.noreply.github.com>
2023-09-21 23:29:38 +0200

    изменения стиля

--- main-service/src/main/java/ru/practicum/category/repository/CategoryRepository.java
+++ main-service/src/main/java/ru/practicum/category/repository/CategoryRepository.java
@@ -3,6 +3,7 @@ package ru.practicum.category.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import ru.practicum.category.model.entity.Category;
+
 @Component
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
